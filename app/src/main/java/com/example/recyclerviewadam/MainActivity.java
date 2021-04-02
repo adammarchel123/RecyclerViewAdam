79d4f4ceafd348829898ad29cde39bdea4a3cbc8
adammarchel123 <adammarchel123@gmail.com>
2021-04-02 20:59:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewadam/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.recyclerviewadam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1;
+    String[] s2;
+    int[] images = {
+            R.drawable.cplusplus,
+            R.drawable.csharp,
+            R.drawable.java,
+            R.drawable.javascript,
+            R.drawable.kotlinnn,
+            R.drawable.python,
+            R.drawable.ruby,
+            R.drawable.swift,
+            R.drawable.typescript,
+            R.drawable.visualstudio
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.programming_languages);
+        s2 = getResources().getStringArray(R.array.description);
+
+
+    }
+}
